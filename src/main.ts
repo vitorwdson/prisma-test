563215a36fa24a084eced90828b49a7fbb615c4c
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-21 22:00:12 -0400

    Disabled yarn 3 PnP

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany();
+
+  console.log(allUsers);
 }
 
 main()
