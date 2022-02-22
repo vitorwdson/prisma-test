9389845ec175bfff30cae4e7a20bf25402f201a7
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-21 22:12:22 -0400

    Updating data from the database

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,12 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
-      },
-      profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
   });
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
-
-  console.dir(allUsers, { depth: null });
+  console.log(post);
 }
 
 main()
